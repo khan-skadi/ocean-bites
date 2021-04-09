a8bd67e733b2dacb9a77e1b0539dd40a47d9e2e9
Petar Kartalov <kartalov.pt@gmail.com>
2021-04-10 01:54:31 +0200

    add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
 import React from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import Aos from "aos"
+import "App.css"
+import "aos/dist/aos.css"
 
-function App(): JSX.Element {
+const App = (): JSX.Element => {
+  React.useEffect(() => {
+    Aos.init({ duration: 2000 })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="appWrapper">
+      <nav>
+        <div style={{ display: "none" }} />
+      </nav>
+      <div className="section">
+        <div className="landing-main">
+          <div className="landing-bg" />
+
+          <div className="landing-nav">
+            <div className="landing-nav-wrapper">
+              <p>Ocean Bites</p>
+            </div>
+          </div>
+
+          <div className="landing-main-inner">
+            <div className="hero-container">
+              <div className="hero-wrapper">
+                <p className="hero-text">
+                  <span>where great food and great drinks meet</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section">App</div>
     </div>
   )
 }
