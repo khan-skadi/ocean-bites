36f17bb8d7b5a6f6410397e79b68447117dc3c60
Petar Kartalov <kartalov.pt@gmail.com>
2021-05-09 01:15:47 +0200

    resp ourStory

--- src/components/Home/TestLandingPage.tsx
+++ src/components/Home/TestLandingPage.tsx
@@ -10,7 +10,7 @@ import quote from "assets/images/quote.svg";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
-    root: {
+    hero: {
       position: "relative",
     },
     heroWrapper: {
@@ -27,6 +27,7 @@ const useStyles = makeStyles((theme: Theme) =>
         height: "100%",
         objectFit: "cover",
         objectPosition: "center right",
+        filter: "brightness(75%)",
       },
     },
     heroCtaWrapper: {
@@ -36,7 +37,6 @@ const useStyles = makeStyles((theme: Theme) =>
       width: "100%",
       height: "100%",
       position: "absolute",
-      background: "#000",
       top: 140,
       right: 10,
 
@@ -49,15 +49,15 @@ const useStyles = makeStyles((theme: Theme) =>
       display: "flex",
       alignItems: "center",
       justifyContent: "center",
-      width: "130px",
+      width: "140px",
       height: "60px",
       textAlign: "center",
       border: "1px solid #fff",
       borderRadius: "50%",
 
       [theme.breakpoints.up("sm")]: {
-        width: 294,
-        height: 175,
+        width: 295,
+        height: 140,
       },
 
       "& p": {
@@ -67,9 +67,60 @@ const useStyles = makeStyles((theme: Theme) =>
         color: "#fff",
         margin: 0,
         padding: 0,
+        width: "100%",
 
         [theme.breakpoints.up("sm")]: {
+          width: "90%",
           fontSize: "22px",
+        },
+      },
+    },
+    ourStory: {
+      position: "relative",
+      background: "#000",
+      top: 150,
+      padding: "15px 0",
+
+      [theme.breakpoints.up("sm")]: {
+        top: 600,
+      },
+    },
+    ourStoryWrapper: {
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    ourStoryTitle: {
+      fontSize: "18px",
+      textTransform: "uppercase",
+      whiteSpace: "nowrap",
+      color: "#fff",
+      fontFamily: "Grotesk",
+      marginRight: theme.spacing(2),
+      paddingLeft: theme.spacing(2),
+
+      [theme.breakpoints.up("sm")]: {
+        marginRight: theme.spacing(4),
+        paddingLeft: theme.spacing(4),
+        fontSize: "26px",
+      },
+    },
+    ourStoryText: {
+      color: "#fff",
+      fontFamily: "Grotesk",
+
+      "& p": {
+        fontSize: "10px",
+        margin: 0,
+        marginTop: theme.spacing(0.5),
+
+        [theme.breakpoints.up("sm")]: {
+          fontSize: "16px",
+          marginRight: theme.spacing(4),
+        },
+
+        "&:last-of-type": {
+          marginTop: "20px",
         },
       },
     },
@@ -80,16 +131,28 @@ const TestLandingPage = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div className={classes.heroWrapper}>
-        <img src={hero} alt="" />
-      </div>
-      <div className={classes.heroCtaWrapper}>
-        <div className={classes.heroCta}>
-          <p>Where great food and great drinks meet</p>
+    <>
+      <section className={classes.hero}>
+        <div className={classes.heroWrapper}>
+          <img src={hero} alt="" />
         </div>
-      </div>
-    </div>
+        <div className={classes.heroCtaWrapper}>
+          <div className={classes.heroCta}>
+            <p>Where great food and great drinks meet</p>
+          </div>
+        </div>
+      </section>
+      <section className={classes.ourStory}>
+        <div className={classes.ourStoryWrapper}>
+          <p className={classes.ourStoryTitle}>Our Story</p>
+          <div className={classes.ourStoryText}>
+            <p>We have been serving irresistible comfort food since 1985.</p>
+            <p>Our mission is to keep you smiling with every bite.</p>
+            <p>Treat yourself to a feel-good meal today!</p>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
