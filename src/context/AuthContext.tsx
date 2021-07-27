f84ea940a5124cf7ba2c6a5d538af15014c585b2
Petar Kartalov <kartalov.pt@gmail.com>
2021-07-27 19:25:13 +0200

    Update AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,7 +3,10 @@ import { createContext } from "utils/createContext";
 import firebase, { auth } from "config/firebase";
 
 interface AuthContextValues {
+  isAuthenticated: boolean;
   currentUser: firebase.User | null;
+  revalidate: () => void;
+  logout: () => Promise<boolean>;
   login: (email: string, password: string) => Promise<firebase.auth.UserCredential>;
 }
 
@@ -13,22 +16,66 @@ const [Provider, useAuthContext] = createContext<AuthContextValues>({
 
 const AuthContextProvider: FC = ({ children }): JSX.Element => {
   const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
-
-  const login = (email: string, password: string): Promise<firebase.auth.UserCredential> =>
-    auth.signInWithEmailAndPassword(email, password);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [token, setToken] = useState("");
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
+    const unsubscribe: () => void = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
+
+      if (user) {
+        user.getIdToken().then((authToken) => setToken(authToken));
+      } else {
+        setToken("");
+      }
     });
 
     return unsubscribe;
   }, []);
 
+  useEffect(() => {
+    revalidate();
+  });
+
+  const login = (email: string, password: string): Promise<firebase.auth.UserCredential> => {
+    const user = auth.signInWithEmailAndPassword(email, password);
+    setIsAuthenticated(true);
+    localStorage.setItem("token", token);
+
+    return user;
+  };
+
+  const logout = async () => {
+    try {
+      await auth.signOut();
+      setIsAuthenticated(false);
+      localStorage.removeItem("token");
+
+      return true;
+    } catch (err) {
+      return false;
+    }
+  };
+
+  const revalidate = () => {
+    const user = auth.currentUser;
+
+    if (user) {
+      setCurrentUser(user);
+      setIsAuthenticated(true);
+    } else {
+      setCurrentUser(null);
+      setIsAuthenticated(false);
+    }
+  };
+
   return (
     <Provider
       value={{
+        isAuthenticated,
         currentUser,
+        revalidate,
+        logout,
         login,
       }}
     >
