79acaae3e150735c4efc436b6a05da005903ff2e
Petar Kartalov <kartalov.pt@gmail.com>
2021-07-27 19:24:50 +0200

    Update Login component alert messages to use redux alerts

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { CssBaseline } from "@material-ui/core";
+import { store } from "store";
 import theme from "utils/theme";
 import dotenv from "dotenv";
 import App from "./App";
@@ -13,10 +15,12 @@ dotenv.config();
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { useAuthContext } from "context/AuthContext";
 import { useFormik, FormikValues } from "formik";
@@ -7,10 +8,10 @@ import { makeStyles, Theme } from "@material-ui/core/styles";
 // Components
 import { Button } from "@material-ui/core";
 import { Text } from "components/FormFields";
-import Alert, { AlertProps } from "components/Alert";
 
 // Helpers
 import { PATHS } from "utils/appConstants";
+import { setAlertProps } from "store";
 import { ALERT_MESSAGES } from "utils/verbiage";
 import { getFormikFieldProps } from "utils/getFormikFieldProps";
 import { VALIDATION_SCHEMA } from "./constants";
@@ -46,31 +47,33 @@ const useStyles = makeStyles((theme: Theme) => ({
 const Login = () => {
   const classes = useStyles();
   const history = useHistory();
-  const { currentUser, login } = useAuthContext();
-  const [alertProps, setAlertProps] = useState<Partial<AlertProps>>({
-    open: false,
-  });
+  const dispatch = useDispatch();
+  const { login } = useAuthContext();
 
   const onSubmit = (values: FormikValues) =>
     login(values.email, values.password)
       .then((res) => {
         if (res.user) {
-          setAlertProps({
-            open: true,
-            onClose: () => setAlertProps({ open: false }),
-            severity: "success",
-            message: ALERT_MESSAGES.formSubmittedSuccessfully,
-          });
+          dispatch(
+            setAlertProps({
+              open: true,
+              onClose: () => dispatch(setAlertProps({ open: false })),
+              severity: "success",
+              message: ALERT_MESSAGES.loggedInSuccessfully,
+            })
+          );
           history.push(PATHS.menu);
         }
       })
       .catch((err) => {
-        setAlertProps({
-          open: true,
-          onClose: () => setAlertProps({ open: false }),
-          severity: "error",
-          message: err.message || ALERT_MESSAGES.errorSubmittingForm,
-        });
+        dispatch(
+          setAlertProps({
+            open: true,
+            onClose: () => dispatch(setAlertProps({ open: false })),
+            severity: "error",
+            message: err.message || ALERT_MESSAGES.errorSubmittingForm,
+          })
+        );
       });
 
   const formik = useFormik({
@@ -83,11 +86,8 @@ const Login = () => {
   const { values, touched, errors, handleSubmit } = formik;
   const formikFieldProps = getFormikFieldProps(formik);
 
-  console.log("currentUser: ", currentUser);
-
   return (
     <div className={classes.root}>
-      <Alert {...alertProps} />
       <p className={classes.titleText}>Login to Ocean Bites</p>
 
       <form className={classes.form} onSubmit={handleSubmit}>
